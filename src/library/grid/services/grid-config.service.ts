e9c58fc37a180cf1cad838e59f9dd5e1d52afba3
Yohan Gomez <yohan.gz@hotmail.com>
2017-12-10 12:09:26 +0530

    Add grid config service

---
+++ src/library/grid/services/grid-config.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+/**
+ * This service class hold all the global configurations of grid which can be overridden while importing the module.
+ */
+@Injectable()
+export class GridConfig {
+
+}
