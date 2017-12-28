c7b1107652ecf11afde434792c2acbc2d3380c30
Yohan Gomez <yohan.gz@hotmail.com>
2017-12-28 19:13:20 +0530

    Export dropdown and utility

--- src/library/index.ts
+++ src/library/index.ts
@@ -1,3 +1,8 @@
 export { GridConfigService } from './grid/services/grid-config.service';
 export { GridComponent } from './grid/components/grid/grid.component';
 export { GridModule } from './grid/grid.module';
+
+export { DropdownConfigService } from './dropdown/services/dropdown-config.service';
+export { DropdownModule } from './dropdown/dropdown.module';
+
+export { UtilityModule } from './utility/utility.module';
