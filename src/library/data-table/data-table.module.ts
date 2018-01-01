e398d21edb46703067ff0fb007e44e387d781231
Yohan Gomez <yohan.gz@hotmail.com>
2018-01-01 21:56:46 +0530

    Update data table module

---
+++ src/library/data-table/data-table.module.ts
@@ -0,0 +1,62 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { DataTableColumnComponent } from './components/data-table-column/data-table-column.component';
+import { DataTableHeaderComponent } from './components/data-table-header/data-table-header.component';
+import { DataTableColGroupComponent } from './components/data-table-col-group/data-table-col-group.component';
+import { DataTablePaginationComponent } from './components/data-table-pagination/data-table-pagination.component';
+import { DataTableComponent } from './components/data-table/data-table.component';
+import { DataTableBodyComponent } from './components/data-table-body/data-table-body.component';
+
+import { ScrollPosition } from './directives/scroll-position.directive';
+import { ScrollElement } from './directives/scroll-element.directive';
+import { ElementWidth } from './directives/element-width.directive';
+
+import { DataTableConfigService } from './services/data-table-config.service';
+
+import { DropdownModule } from '../dropdown';
+import { UtilityModule } from '../utility';
+
+const COMPONENTS = [
+  DataTableHeaderComponent,
+  DataTableColumnComponent,
+  DataTableColGroupComponent,
+  DataTableBodyComponent,
+  DataTablePaginationComponent,
+  DataTableComponent
+];
+
+const DIRECTIVES = [
+  ScrollPosition,
+  ScrollElement,
+  ElementWidth
+];
+
+/***
+ * Module representing grid component.
+ */
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    UtilityModule.forRoot(),
+    DropdownModule.forRoot()
+  ],
+  declarations: [
+    ...COMPONENTS,
+    ...DIRECTIVES
+  ],
+  exports: [
+    DataTableColumnComponent,
+    DataTableComponent
+  ]
+})
+export class DataTableModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: DataTableModule,
+      providers: [DataTableConfigService]
+    };
+  }
+}
