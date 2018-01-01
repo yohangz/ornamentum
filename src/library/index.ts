7760f8ee8a16c723967bb443054779ae48b36b01
Yohan Gomez <yohan.gz@hotmail.com>
2018-01-01 21:57:25 +0530

    Add library index

--- src/library/index.ts
+++ src/library/index.ts
@@ -1,8 +1,3 @@
-export { GridConfigService } from './grid/services/grid-config.service';
-export { GridComponent } from './grid/components/grid/grid.component';
-export { GridModule } from './grid/grid.module';
-
-export { DropdownConfigService } from './dropdown/services/dropdown-config.service';
-export { DropdownModule } from './dropdown/dropdown.module';
-
-export { UtilityModule } from './utility/utility.module';
+export * from './data-table';
+export * from './dropdown';
+export * from './utility/';
