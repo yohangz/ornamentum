b1d26c889ba880718ecdda3a0f921584c976d933
Yohan Gomez <yohan.gz@hotmail.com>
2018-01-01 21:57:06 +0530

    Add data table index

---
+++ src/library/data-table/index.ts
@@ -0,0 +1,4 @@
+export * from './services/data-table-config.service';
+export * from './models/data-table.model';
+export * from './models/data-table-sort-order.enum';
+export * from './data-table.module';
