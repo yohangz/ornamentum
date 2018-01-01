5ec59790ab16b4c751de14b1dde98f70a81adf8b
Yohan Gomez <yohan.gz@hotmail.com>
2018-01-01 21:54:30 +0530

    Add data table column group component.

---
+++ src/library/data-table/components/data-table-col-group/data-table-col-group.component.ts
@@ -0,0 +1,18 @@
+import { Component, forwardRef, Inject } from '@angular/core';
+
+import { DataTableComponent } from '../data-table/data-table.component';
+
+/**
+ * Data table column group component.
+ * @class DataTableColGroup
+ */
+@Component({
+  exportAs: 'appDataTableColGroup',
+  selector: '[appDataTableColGroup]',
+  styleUrls: [ './data-table-col-group.component.scss' ],
+  templateUrl: './data-table-col-group.component.html'
+})
+export class DataTableColGroupComponent {
+  constructor(@Inject(forwardRef(() => DataTableComponent)) public dataTable: DataTableComponent) {
+  }
+}
