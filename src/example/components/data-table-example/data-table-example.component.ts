3d16db1bd883ff4c2d2bf3e9459438d2f1f94e4b
Lahiru Jayamanna <lsampath999@gmail.com>
2018-01-03 08:49:30 +0530

    Add external configurability support for available features in the grid.

--- src/example/components/data-table-example/data-table-example.component.ts
+++ src/example/components/data-table-example/data-table-example.component.ts
@@ -1,9 +1,96 @@
 import { Component } from '@angular/core';
 
+import { DataTableColumnComponent } from '../../../library/data-table/components/data-table-column/data-table-column.component';
+
+import { SortOrder } from '../../../library/data-table/models/data-table-sort-order.enum';
+import { DataTableParams, DataTableTranslations } from '../../../library/data-table/models/data-table.model';
+
+import { DataTableResource } from '../../../library/data-table/services/data-table-resource';
+
+declare function require(url: string);
+
+const data = require('../../data/data.json');
+
 @Component({
   selector: 'app-data-table-example-component',
   templateUrl: './data-table-example.component.html',
   styleUrls: ['./data-table-example.component.scss']
 })
 export class DataTableExampleComponent {
+  gridTitle: string;
+  autoFetch: boolean;
+  expandableRows: boolean;
+  filterDebounce: boolean;
+  multiRowSelectable: boolean;
+  pageable: boolean;
+  rowSelectable: boolean;
+  selectOnRowClick: boolean;
+  showHeader: boolean;
+  showIndexColumn: boolean;
+  showLoadingSpinner: boolean;
+  showSubstituteRows: boolean;
+  filterDebounceTime: number;
+  indexColumnTitle: string;
+  contentHeight: string;
+  itemCount: number;
+  items: any[];
+  limit: number;
+  page: number;
+  tableResource: DataTableResource<any>;
+  translations: DataTableTranslations;
+
+  constructor() {
+    this.gridTitle = 'Algorithms Grid';
+    this.autoFetch = true;
+    this.expandableRows = true;
+    this.filterDebounce = true;
+    this.multiRowSelectable = false;
+    this.pageable = true;
+    this.rowSelectable = true;
+    this.selectOnRowClick = true;
+    this.showHeader = true;
+    this.showIndexColumn = true;
+    this.showLoadingSpinner = true;
+    this.showSubstituteRows = true;
+    this.filterDebounceTime = 500;
+    this.contentHeight = '300px';
+    this.indexColumnTitle = '#';
+    this.limit = 10;
+    this.page = 1;
+    this.translations = {
+      noDataMessageBody: 'ඩේටා නොමැත. සමාවන්න !!',
+      noDataMessageHeader: 'Header'
+    };
+    this.items = [];
+
+    this.fetchAlgorithmsData();
+  }
+
+  /**
+   * Data table refresh event handler.
+   * @param params  Event payload.
+   */
+  public onRefresh(params?: any): void {
+    this.fetchAlgorithmsData();
+    this.tableResource.query(params).then((items) => this.items = items);
+  }
+
+  /**
+   * Data table data load event handler.
+   * @param {DataTableParams} params  grid parameters.
+   */
+  public onDataLoad(params?: any): void {
+    this.tableResource.query(params).then((items) => this.items = items);
+  }
+
+  /**
+   * Fetch algorithms data and bind to the data table source.
+   */
+  private fetchAlgorithmsData(): void {
+    this.tableResource = new DataTableResource();
+    this.tableResource.items = data;
+
+    this.tableResource.count().then((count) => this.itemCount = count);
+  }
 }
+
