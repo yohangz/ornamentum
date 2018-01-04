6602f4754e0654a7144b1a7963d011b481926d6d
samudithaw <samudithaw@zone24x7.com>
2018-01-04 17:29:22 +0530

    Updating Data Table Header Dropdown Styles

--- src/library/data-table/components/data-table-column-selector/data-table-column-selector.component.ts
+++ src/library/data-table/components/data-table-column-selector/data-table-column-selector.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 import { ColumnSelectorProperty } from '../../models/data-table.model';
 
 @Component({
-  selector: 'app-test',
+  selector: 'app-data-table-column-selector',
+  styleUrls: [ './data-table-column-selector.component.scss' ],
   templateUrl: './data-table-column-selector.component.html'
 })
 export class DataTableColumnSelectorComponent {
