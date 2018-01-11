22cbda135e5e54a5ff8402fcf7350fc6c8697c0d
Yohan Gomez <yohan.gz@hotmail.com>
2018-01-12 00:23:32 +0530

    Expose dropdown component

--- src/library/dropdown/index.ts
+++ src/library/dropdown/index.ts
@@ -1,3 +1,4 @@
 export * from './services/dropdown-config.service';
 export * from './models/dropdown.model';
 export * from './dropdown.module';
+export * from './components/dropdown/dropdown.component';
