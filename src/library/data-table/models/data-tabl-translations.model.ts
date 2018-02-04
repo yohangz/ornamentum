4c3425ba3d775a3a9fa89219c658feb6fe658dae
Yohan Gomez <yohan.gz@hotmail.com>
2018-02-04 16:42:19 +0530

    Add data table translations model

---
+++ src/library/data-table/models/data-tabl-translations.model.ts
@@ -0,0 +1,46 @@
+/**
+ * Data table translations.
+ */
+export interface DataTableTranslations {
+  /**
+   * Index column title.
+   * @type {number}
+   */
+  indexColumn?: string;
+
+  /**
+   * Select checkbox column title.
+   * @type {string}
+   */
+  selectColumn?: string;
+
+  /**
+   * Expand column title.
+   * @type {string}
+   */
+  expandColumn?: string;
+
+  /**
+   * Pagination limit label title.
+   * @type {string}
+   */
+  paginationLimit?: string;
+
+  /**
+   * Pagination range label title.
+   * @type {string}
+   */
+  paginationRange?: string;
+
+  /**
+   * No data message body.
+   * @type {string}
+   */
+  noDataMessageBody?: string;
+
+  /**
+   * No data message header.
+   * @type {string}
+   */
+  noDataMessageHeader?: string;
+}
