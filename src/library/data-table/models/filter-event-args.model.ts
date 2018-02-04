9c92dcb51bbe8a89a34dc26f1d6d938ccfbf5a55
Yohan Gomez <yohan.gz@hotmail.com>
2018-02-04 18:45:13 +0530

    Add filter event args model

---
+++ src/library/data-table/models/filter-event-args.model.ts
@@ -0,0 +1,18 @@
+import { DataTableColumnComponent } from '../components/data-table-column/data-table-column.component';
+
+/**
+ * Filter event arguments.
+ */
+export interface FilterEventArgs {
+  /**
+   * Target column.
+   * @type {DataTableColumnComponent}
+   */
+  column: DataTableColumnComponent;
+
+  /**
+   * Filter value.
+   * @type {any}
+   */
+  filter: any;
+}
