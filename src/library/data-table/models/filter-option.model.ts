7db8b2d215df65894dd7413c3de054f0cfeb56d8
Yohan Gomez <yohan.gz@hotmail.com>
2018-02-04 19:03:09 +0530

    Add filter option model.

---
+++ src/library/data-table/models/filter-option.model.ts
@@ -0,0 +1,16 @@
+/**
+ * Filter options.
+ */
+export interface FilterOption {
+  /**
+   * Filter item key.
+   * @type {any}
+   */
+  key: any;
+
+  /**
+   * Display filter value.
+   * @type {string}
+   */
+  value: string;
+}
