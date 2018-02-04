779523c051f78a0ae47ea4b3a2f6a3aae046f8c4
Yohan Gomez <yohan.gz@hotmail.com>
2018-02-04 16:33:27 +0530

    Add column selector property model

---
+++ src/library/data-table/models/column-selector-property.model.ts
@@ -0,0 +1,12 @@
+import { DataTableComponent } from '../components/data-table/data-table.component';
+
+/**
+ * Column selector property.
+ */
+export interface ColumnSelectorProperty {
+  /**
+   * Column selector parent data table.
+   * @type {DataTableComponent}
+   */
+  dataTable: DataTableComponent;
+}
