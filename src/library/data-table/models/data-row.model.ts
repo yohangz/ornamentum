c8c3b638a76b30213abd278ae45020582be815ec
Yohan Gomez <yohan.gz@hotmail.com>
2018-02-04 16:37:43 +0530

    Add data table row model.

---
+++ src/library/data-table/models/data-row.model.ts
@@ -0,0 +1,52 @@
+/**
+ * Data table row
+ */
+export interface DataRow {
+  /**
+   * Source data item reference.
+   * @type {any}
+   */
+  item: any;
+
+  /**
+   * Row selected state.
+   * @type {boolean}
+   */
+  selected: boolean;
+
+  /**
+   * Row disabled state.
+   * @type {boolean}
+   */
+  disabled: boolean;
+
+  /**
+   * Row colour hex.
+   * @type {string}
+   */
+  colour: string;
+
+  /**
+   * Row tooltip text.
+   * @type {string}
+   */
+  tooltip: string;
+
+  /**
+   * Row auto generated index.
+   * @type {number}
+   */
+  index: number;
+
+  /**
+   * Row expanded state.
+   * @type {boolean}
+   */
+  expanded: boolean;
+
+  /**
+   * Row data loaded state.
+   * @type {boolean}
+   */
+  dataLoaded: boolean;
+}
