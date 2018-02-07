42d50fa68ef441e8207ce57108cb60c00358412e
Yohan Gomez <yohan.gz@hotmail.com>
2018-02-07 23:33:13 +0530

    Add SortComparatorCallback model

---
+++ src/library/data-table/models/sort-comparator-callback.model.ts
@@ -0,0 +1,7 @@
+/**
+ * Sort comparator callback.
+ * @param {any} previousItem - Previous item.
+ * @param {any} nextItem - Next item.
+ * @return {number} - Sort comparator value.
+ */
+export type SortComparatorCallback = (previousItem: any, nextItem: any) => number;
