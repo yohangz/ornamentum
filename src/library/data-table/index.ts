1905d26aad4e680145577e0610ab83613d9f34df
Yohan Gomez <yohan.gz@hotmail.com>
2018-02-07 23:44:02 +0530

    Export new models

--- src/library/data-table/index.ts
+++ src/library/data-table/index.ts
@@ -1,8 +1,35 @@
-export * from './components/data-table/data-table.component';
-export * from './components/data-table-column/data-table-column.component';
-export * from './services/data-table-config.service';
-export * from './services/data-table-resource.class';
-export * from './services/data-table-resource-factory.service';
-export * from './models/data-table.model';
-export * from './models/data-table-sort-order.enum';
-export * from './data-table.module';
+export { SortOrder } from './models/sort-order.enum';
+export { StorageMode } from './models/storage-mode.enum'
+
+export { CellBindEventArgs } from './models/cell-bind-event-args.model';
+export { QueryResult } from './models/query-result.model';
+export { FilterColumn } from './models/filter-column.model';
+export { SortColumn } from './models/sort-column.model';
+export { FilterOption } from './models/filter-option.model';
+export { CellClickEventArgs } from './models/cell-click-event-args.model';
+export { HeaderClickEventArgs } from './models/header-click-event-args.model';
+export { DoubleClickEventArgs } from './models/double-click-event-args.model';
+export { RowClickEventArgs } from './models/row-click-event-args.model';
+export { RowSelectEventArgs } from './models/row-select-event-args.model';
+export { ScrollPoint } from './models/scroll-point.model';
+export { DataRow } from './models/data-row.model';
+export { DataTableParams } from './models/data-table-params.model';
+export { DataTableTranslations } from './models/data-tabl-translations.model';
+export { GroupFieldExtractorCallback } from './models/group-field-extractor-callback.model';
+export { FilterValueExtractCallback } from './models/filter-value-extract-callback.model';
+export { FilterValueFormatterCallback } from './models/filter-value-formatter-callback.model';
+export { FilterFieldMapperCallback } from './models/filter-field-mapper-callback.model';
+export { SortComparatorCallback } from './models/sort-comparator-callback.model';
+export { FilterExpressionCallback } from './models/filter-expression-callback.model';
+export { CellColourRenderCallback } from './models/cell-colour-render-callback.model';
+
+export { DataTableComponent } from './components/data-table/data-table.component';
+export { DataTableColumnComponent } from './components/data-table-column/data-table-column.component';
+
+export { DataTableResource} from './services/data-table-resource.class';
+
+export { DataTableConfigService } from './services/data-table-config.service';
+export { DataTableResourceFactoryService } from './services/data-table-resource-factory.service';
+
+export { DataTableModule } from './data-table.module';
+export { DataTableResourceManager } from './services/data-table-resource-manager.class';
