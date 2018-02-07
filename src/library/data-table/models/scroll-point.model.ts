7a4fc38f052ed6f2fae47c8e1daeb914a45ceaee
Yohan Gomez <yohan.gz@hotmail.com>
2018-02-07 23:26:20 +0530

    Add ScrollPoint model

---
+++ src/library/data-table/models/scroll-point.model.ts
@@ -0,0 +1,16 @@
+/**
+ * Scroll position point.
+ */
+export interface ScrollPoint {
+  /**
+   * Scroll left pixel count.
+   * @type {number}
+   */
+  scrollLeft: number;
+
+  /**
+   * Scroll top pixel count.
+   * @type {number}
+   */
+  scrollTop: number;
+}
