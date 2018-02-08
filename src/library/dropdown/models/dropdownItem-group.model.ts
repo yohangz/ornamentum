6a28eda4ec469031e312c260409af80323534079
Yohan Gomez <yohan.gz@hotmail.com>
2018-02-08 17:58:29 +0530

    Add DropdownItemGroup model

---
+++ src/library/dropdown/models/dropdownItem-group.model.ts
@@ -0,0 +1,18 @@
+import {DropdownItem} from './dropdown-item.model';
+
+/**
+ * Dropdown item group.
+ */
+export interface DropdownItemGroup {
+  /**
+   * Group name.
+   * @type {string}
+   */
+  groupName: string;
+
+  /**
+   * Group items.
+   * @type {DropdownItem[]}
+   */
+  items: DropdownItem[];
+}
