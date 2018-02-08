f443596c1fda37cffc46f91e16d55a22bb97ad89
Yohan Gomez <yohan.gz@hotmail.com>
2018-02-08 18:02:33 +0530

    Add DropdownViewProperty model

---
+++ src/library/dropdown/models/dropdown-view-property.model.ts
@@ -0,0 +1,12 @@
+import {DropdownComponent} from '../';
+
+/**
+ * Dropdown view property.
+ */
+export interface DropdownViewProperty {
+  /**
+   * Dropdown component reference.
+   * @type {DropdownComponent}
+   */
+  dropdown: DropdownComponent;
+}
