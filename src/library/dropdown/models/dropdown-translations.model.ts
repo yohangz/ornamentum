2e14cc7785f04aa5e33081b6f4a7e69a0ac1e8f5
Yohan Gomez <yohan.gz@hotmail.com>
2018-02-08 17:43:04 +0530

    Add DropdownTranslations model

---
+++ src/library/dropdown/models/dropdown-translations.model.ts
@@ -0,0 +1,35 @@
+/**
+ * Dropdown translations.
+ */
+export interface DropdownTranslations {
+  /**
+   * Select placeholder.
+   * @type {string}
+   */
+  selectPlaceholder?: string;
+
+  /**
+   * Search placeholder.
+   * @type {string}
+   */
+  searchPlaceholder?: string;
+
+  /**
+   * Search empty result text.
+   * @type {string}
+   */
+  searchEmptyResult?: string;
+
+  /**
+   * Select all checkbox placeholder.
+   * @type {string}
+   */
+  selectAllPlaceholder?: string;
+
+  /**
+   * Selected items wrap placeholder.
+   * Used when item count > displaySelectedLimit
+   * @type {string}
+   */
+  selectedItemWrapPlaceholder?: string;
+}
