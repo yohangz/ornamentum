36d07c050bb4565c0e423ee0be71c7633e649935
Yohan Gomez <yohan.gz@hotmail.com>
2018-02-18 12:25:58 +0530

    Fix global reference service lint issues

--- src/library/utility/services/global-ref.service.ts
+++ src/library/utility/services/global-ref.service.ts
@@ -13,20 +13,20 @@ export class GlobalRefService {
   }
 
   private setScrollbarWidth(): void {
-    const outer = document.createElement("div");
-    outer.style.visibility = "hidden";
-    outer.style.width = "100px";
-    outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
+    const outer = document.createElement('div');
+    outer.style.visibility = 'hidden';
+    outer.style.width = '100px';
+    outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
 
     document.body.appendChild(outer);
 
     const widthNoScroll = outer.offsetWidth;
     // force scrollbars
-    outer.style.overflow = "scroll";
+    outer.style.overflow = 'scroll';
 
     // add innerdiv
-    const inner = document.createElement("div");
-    inner.style.width = "100%";
+    const inner = document.createElement('div');
+    inner.style.width = '100%';
     outer.appendChild(inner);
 
     const widthWithScroll = inner.offsetWidth;
