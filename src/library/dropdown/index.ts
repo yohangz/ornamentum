0474f324f6d4e81e93f1405ec56bb70a721c24b8
Yohan Gomez <yohan.gz@hotmail.com>
2018-02-18 12:27:54 +0530

    Fix dropdown module exports

--- src/library/dropdown/index.ts
+++ src/library/dropdown/index.ts
@@ -1,10 +1,11 @@
-export { DropdownConfigService } from './services/dropdown-config.service';
-
-export { DropdownComponent } from './components/dropdown/dropdown.component';
-
 export { MenuPosition } from './models/menu-position.enum';
-export { DropdownModule } from './dropdown.module';
 export { DropdownTranslations } from './models/dropdown-translations.model';
 export { DropdownItem } from './models/dropdown-item.model';
 export { DataRequestParams } from './models/data-request-params.model';
 export { ClientFilterCallback } from './models/client-filter-callback.model';
+
+export { DropdownComponent } from './components/dropdown/dropdown.component';
+
+export { DropdownConfigService } from './services/dropdown-config.service';
+
+export { DropdownModule } from './dropdown.module';
