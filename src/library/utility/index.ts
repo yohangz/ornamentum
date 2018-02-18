25815d090498abf89fffbb463f1fd64af47b8e29
Yohan Gomez <yohan.gz@hotmail.com>
2018-02-18 12:27:25 +0530

    Fix utility module exports

--- src/library/utility/index.ts
+++ src/library/utility/index.ts
@@ -1,6 +1,8 @@
-export * from './services/drag-and-drop.service';
-export * from './services/global-ref.service';
-export * from './services/utility-config.service';
-export * from './services/component-loader.class';
-export * from './services/component-loader-factory.service';
-export * from './utility.module';
+export { DragAndDropService } from './services/drag-and-drop.service';
+export { GlobalRefService } from './services/global-ref.service';
+export { UtilityConfigService } from './services/utility-config.service';
+export { ComponentLoader } from './services/component-loader.interface';
+export { PopoverComponentLoader } from './services/popover-component-loader.class';
+export { PopoverComponentLoaderFactoryService } from './services/popover-component-loader-factory.service';
+
+export { UtilityModule } from './utility.module';
