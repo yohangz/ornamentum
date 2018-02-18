5442daa52e74f7c003ffde9040597afd6a9b6cdf
Yohan Gomez <yohan.gz@hotmail.com>
2018-02-18 12:33:26 +0530

    Update props references to config

--- src/library/data-table/components/data-table-column-selector/data-table-column-selector.component.ts
+++ src/library/data-table/components/data-table-column-selector/data-table-column-selector.component.ts
@@ -1,11 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+import { DataTableConfigService } from '../../services/data-table-config.service';
+
+import { DataTableColumnComponent } from '../data-table-column/data-table-column.component';
 
 @Component({
   selector: 'ng-data-table-column-selector',
   styleUrls: ['./data-table-column-selector.component.scss'],
-  templateUrl: './data-table-column-selector.component.html'
+  templateUrl: './data-table-column-selector.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DataTableColumnSelectorComponent {
   @Input()
-  public props: any;
+  public columns: DataTableColumnComponent;
+
+  public expandableRows: boolean;
+  public showIndexColumn: boolean;
+  public rowSelectable: boolean;
+
+  constructor(public config: DataTableConfigService) {
+    this.expandableRows = config.rowSelectable;
+    this.showIndexColumn = config.showIndexColumn;
+    this.rowSelectable = config.rowSelectable;
+  }
 }
