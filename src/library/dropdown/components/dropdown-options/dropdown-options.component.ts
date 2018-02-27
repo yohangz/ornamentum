e219821e5f5a7b4d6a20b2a32763055585d0c55a
Yohan Gomez <yohan.gz@hotmail.com>
2018-02-27 17:51:27 +0530

    Remove unused references

--- src/library/dropdown/components/dropdown-options/dropdown-options.component.ts
+++ src/library/dropdown/components/dropdown-options/dropdown-options.component.ts
@@ -1,12 +1,8 @@
-import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
-
-import { Subject } from 'rxjs/Subject';
-import { Subscription } from 'rxjs/Subscription';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 import { DropdownItem } from '../../models/dropdown-item.model';
 
 import { DropdownDataStateService } from '../../services/dropdown-data-state.service';
-import { DropdownEventStateService } from '../../services/dropdown-event-state.service';
 import { DropdownConfigService } from '../../services/dropdown-config.service';
 
 
