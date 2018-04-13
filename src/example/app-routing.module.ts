7cd6b49660e0202414c5d51548171b8450e0b89b
Yohan Gomez <yohan.gz@hotmail.com>
2018-04-13 11:01:25 +0530

    Publish docs build process updates

--- src/example/app-routing.module.ts
+++ src/example/app-routing.module.ts
@@ -30,7 +30,7 @@ const appRoutes: Routes = [
  */
 @NgModule({
   exports: [RouterModule],
-  imports: [RouterModule.forRoot(appRoutes, { useHash: true })]
+  imports: [RouterModule.forRoot(appRoutes)]
 })
 export class AppRoutingModule {
 }
