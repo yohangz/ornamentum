2f36fd81a170f4d237012ce5bcc95dc8d87ff774
Lahiru Jayamanna <lsampath999@gmail.com>
2018-05-20 17:45:36 +0530

    Update realtime data binding example.

--- src/app/feature/data-table/data-table-feature/components/data-table-real-time-data-binding/data-table-real-time-data-binding.component.ts
+++ src/app/feature/data-table/data-table-feature/components/data-table-real-time-data-binding/data-table-real-time-data-binding.component.ts
@@ -19,29 +19,29 @@ export class DataTableRealTimeDataBindingComponent {
   constructor() {
     this.dataSource = of(sampleData);
 
-    let a = 1;
+    // let a = 1;
 
-    setInterval(() => {
-      this.dataSource.forEach((data: any[]) => {
-        data[0].stationName = `station ${++a}`;
-        data[1].stationName = `station ${++a + 1}`;
-        data[2].stationName = `station ${++a + 3}`;
-        data[3].stationName = `station ${++a + 5}`;
-        data[4].stationName = `station ${++a + 7}`;
-        data[5].stationName = `station ${++a + 9}`;
-
-        data[3].statusValue = `Not In Service ${++a + 1}`;
-        data[4].statusValue = `In Service ${++a + 2}`;
-        data[5].statusValue = `Not In Service ${++a + 3}`;
-
-        data[6].availableBikes = 10 + ++a;
-        data[7].availableBikes = 20 + ++a;
-        data[8].availableBikes = 30 + ++a;
-
-        data[9].stAddress1 = `Address for number ${++a + 9}`;
-        data[10].stAddress1 = `Address for number ${++a + 10}`;
-        data[11].stAddress1 = `Address for number ${++a + 11}`;
-      });
-    }, 1000);
+    // setInterval(() => {
+    //   this.dataSource.forEach((data: any[]) => {
+    //     data[0].stationName = `station ${++a}`;
+    //     data[1].stationName = `station ${++a + 1}`;
+    //     data[2].stationName = `station ${++a + 3}`;
+    //     data[3].stationName = `station ${++a + 5}`;
+    //     data[4].stationName = `station ${++a + 7}`;
+    //     data[5].stationName = `station ${++a + 9}`;
+    //
+    //     data[3].statusValue = `Not In Service ${++a + 1}`;
+    //     data[4].statusValue = `In Service ${++a + 2}`;
+    //     data[5].statusValue = `Not In Service ${++a + 3}`;
+    //
+    //     data[6].availableBikes = 10 + ++a;
+    //     data[7].availableBikes = 20 + ++a;
+    //     data[8].availableBikes = 30 + ++a;
+    //
+    //     data[9].stAddress1 = `Address for number ${++a + 9}`;
+    //     data[10].stAddress1 = `Address for number ${++a + 10}`;
+    //     data[11].stAddress1 = `Address for number ${++a + 11}`;
+    //   });
+    // }, 1000);
   }
 }
