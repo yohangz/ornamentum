edc868843de961a803c2b6ae36607f21a3e7a231
Yohan Gomez <yohan.gz@hotmail.com>
2018-06-09 14:45:54 +0530

    Display package version on main menu

--- src/app/core/components/main-menu/main-menu.component.ts
+++ src/app/core/components/main-menu/main-menu.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
+import { VERSION } from '../../../../environments/version';
+
 @Component({
   selector: 'app-main-menu',
   styleUrls: ['./main-menu.component.scss'],
   templateUrl: './main-menu.component.html'
 })
 export class MainMenuComponent {
+  private packageVersion: string;
+
+  constructor() {
+    this.packageVersion = VERSION;
+  }
 }
