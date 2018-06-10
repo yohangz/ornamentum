f89a636d31237557c1630a355d18c1aa1dfdf934
Yohan Gomez <yohan.gz@hotmail.com>
2018-06-10 17:23:05 +0530

    Sidebar menu filter support

--- src/app/core/components/left-navigation/left-navigation.component.ts
+++ src/app/core/components/left-navigation/left-navigation.component.ts
@@ -1,9 +1,9 @@
-import { Component, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 
 import { Subscription } from 'rxjs/internal/Subscription';
 
-import { MenuGroup, ResizeArgs } from '../../models';
+import { MenuGroup, MenuItem, ResizeArgs } from '../../models';
 import { ContainerResponsiveService } from '../../services';
 
 
@@ -20,7 +20,7 @@ import { ContainerResponsiveService } from '../../services';
     ])
   ]
 })
-export class LeftNavigationComponent implements OnDestroy {
+export class LeftNavigationComponent implements OnInit, OnDestroy {
   private containerResponsiveSubscription: Subscription;
   private navigationToggleSubscription: Subscription;
 
@@ -33,6 +33,7 @@ export class LeftNavigationComponent implements OnDestroy {
   public containerHeight: number;
   public expanded =  true;
   public mobileMode = false;
+  public displayMenuGroups: MenuGroup[];
 
   constructor(private containerResponsive: ContainerResponsiveService) {
     this.containerResponsiveSubscription = this.containerResponsive.containerSize.subscribe((resizeArgs: ResizeArgs) => {
@@ -55,6 +56,29 @@ export class LeftNavigationComponent implements OnDestroy {
     if (this.mobileMode) {
       this.expanded = false;
     }
+  }
+
+  public onSearchKeyUp(element: any): void {
+    const value = element.value;
+
+    this.displayMenuGroups = this.menuGroups.reduce((acc: MenuGroup[], menuGroup: MenuGroup) => {
+      const items = menuGroup.menuItems.filter((menuItem: MenuItem) => {
+        return menuItem.title.toLowerCase().includes(value.toLowerCase());
+      });
+
+      if (items.length) {
+        acc.push({
+          name: menuGroup.name,
+          menuItems: items
+        });
+      }
+
+      return acc;
+    }, []);
+  }
+
+  public ngOnInit(): void {
+    this.displayMenuGroups = this.menuGroups;
   }
 
   public ngOnDestroy(): void {
