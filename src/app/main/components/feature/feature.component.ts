f28d8bd0270cec12a16b55dc98d163f45371e28f
Yohan Gomez <yohan.gz@hotmail.com>
2018-06-11 21:32:42 +0530

    Move route transition animation to main

--- src/app/main/components/feature/feature.component.ts
+++ src/app/main/components/feature/feature.component.ts
@@ -5,30 +5,12 @@ import { Subscription } from 'rxjs/internal/Subscription';
 
 import { MenuGroup, ResizeArgs } from '../../../core/models';
 import { ContainerResponsiveService } from '../../../core/services';
-import { animate, group, query, style, transition, trigger } from '@angular/animations';
 
 
 @Component({
   selector: 'app-feature',
   templateUrl: './feature.component.html',
-  styleUrls: ['./feature.component.scss'],
-  animations: [
-    trigger('routeFadeIn', [
-      transition('* <=> *', [
-        query(':enter, :leave', style({position: 'fixed', width: '100%'}), {optional: true}),
-        group([
-          query(':enter', [
-            style({opacity: 0}),
-            animate('0.5s ease-in-out', style({opacity: 1}))
-          ], {optional: true}),
-          query(':leave', [
-            style({opacity: 1}),
-            animate('0.5s ease-in-out', style({opacity: 0}))
-          ], {optional: true}),
-        ])
-      ])
-    ])
-  ]
+  styleUrls: ['./feature.component.scss']
 })
 export class FeatureComponent  implements OnDestroy {
   private activeRouteSubscription: Subscription;
@@ -38,8 +20,7 @@ export class FeatureComponent  implements OnDestroy {
   public containerHeight: number;
 
   constructor(private activatedRoute: ActivatedRoute,
-              private containerResponsive: ContainerResponsiveService,
-              private router: Router) {
+              private containerResponsive: ContainerResponsiveService) {
     this.activeRouteSubscription = this.activatedRoute.data.subscribe((data: any) => {
       this.navigationData = data.navigation;
     });
@@ -47,10 +28,6 @@ export class FeatureComponent  implements OnDestroy {
     this.containerResponsiveSubscription = this.containerResponsive.containerSize.subscribe((resizeArgs: ResizeArgs) => {
       this.containerHeight = resizeArgs.containerHeight;
     });
-  }
-
-  public getState(): string {
-    return this.router.url;
   }
 
   public ngOnDestroy(): void {
