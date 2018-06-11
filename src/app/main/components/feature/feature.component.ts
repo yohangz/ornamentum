53bed80d4f139043d95bcb911680f1082a4f0270
Yohan Gomez <yohan.gz@hotmail.com>
2018-06-11 21:17:50 +0530

    Route change transition support

--- src/app/main/components/feature/feature.component.ts
+++ src/app/main/components/feature/feature.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Subscription } from 'rxjs/internal/Subscription';
 
 import { MenuGroup, ResizeArgs } from '../../../core/models';
 import { ContainerResponsiveService } from '../../../core/services';
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 
 
 @Component({
   selector: 'app-feature',
   templateUrl: './feature.component.html',
-  styleUrls: ['./feature.component.scss']
+  styleUrls: ['./feature.component.scss'],
+  animations: [
+    trigger('routeFadeIn', [
+      transition('* <=> *', [
+        query(':enter, :leave', style({position: 'fixed', width: '100%'}), {optional: true}),
+        group([
+          query(':enter', [
+            style({opacity: 0}),
+            animate('0.5s ease-in-out', style({opacity: 1}))
+          ], {optional: true}),
+          query(':leave', [
+            style({opacity: 1}),
+            animate('0.5s ease-in-out', style({opacity: 0}))
+          ], {optional: true}),
+        ])
+      ])
+    ])
+  ]
 })
 export class FeatureComponent  implements OnDestroy {
   private activeRouteSubscription: Subscription;
@@ -19,7 +37,9 @@ export class FeatureComponent  implements OnDestroy {
   public navigationData: MenuGroup[];
   public containerHeight: number;
 
-  constructor(private activatedRoute: ActivatedRoute, private containerResponsive: ContainerResponsiveService) {
+  constructor(private activatedRoute: ActivatedRoute,
+              private containerResponsive: ContainerResponsiveService,
+              private router: Router) {
     this.activeRouteSubscription = this.activatedRoute.data.subscribe((data: any) => {
       this.navigationData = data.navigation;
     });
@@ -27,6 +47,10 @@ export class FeatureComponent  implements OnDestroy {
     this.containerResponsiveSubscription = this.containerResponsive.containerSize.subscribe((resizeArgs: ResizeArgs) => {
       this.containerHeight = resizeArgs.containerHeight;
     });
+  }
+
+  public getState(): string {
+    return this.router.url;
   }
 
   public ngOnDestroy(): void {
