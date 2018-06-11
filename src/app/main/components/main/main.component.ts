c7e216873359f8f95e01d2f3cfb901d40343f28c
Yohan Gomez <yohan.gz@hotmail.com>
2018-06-11 21:32:42 +0530

    Move route transition animation to main

--- src/app/main/components/main/main.component.ts
+++ src/app/main/components/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
+import { Router } from '@angular/router';
 
 /**
  * Component class for showing main view.
@@ -7,8 +9,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main',
   styleUrls: ['./main.component.scss'],
-  templateUrl: './main.component.html'
+  templateUrl: './main.component.html',
+  animations: [
+    trigger('routeFadeIn', [
+      transition('* <=> *', [
+        query(':enter, :leave', style({position: 'fixed', width: '100%'}), {optional: true}),
+        group([
+          query(':enter', [
+            style({opacity: 0}),
+            animate('0.5s ease-in-out', style({opacity: 1}))
+          ], {optional: true}),
+          query(':leave', [
+            style({opacity: 1}),
+            animate('0.5s ease-in-out', style({opacity: 0}))
+          ], {optional: true}),
+        ])
+      ])
+    ])
+  ]
 })
 export class MainComponent {
+  constructor(private router: Router) {
+  }
+
+  public getState(): string {
+    return this.router.url;
+  }
 }
 
