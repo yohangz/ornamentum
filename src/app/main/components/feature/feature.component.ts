53a253155e2acb98d1ea68f0f5be571ae1685a2a
Yohan Gomez <yohan.gz@hotmail.com>
2018-06-13 20:15:31 +0530

    Fix data rows getting selected when selectOnRowClick is true and rowSelectable is false.

--- src/app/main/components/feature/feature.component.ts
+++ src/app/main/components/feature/feature.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 import { Subscription } from 'rxjs/internal/Subscription';
 
