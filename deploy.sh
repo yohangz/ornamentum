9a63886dd604c96b7f0cc64054477dae4dbac7ae
Yohan Gomez <yohan.gz@hotmail.com>
2018-06-23 20:06:49 +0530

    Change deploy script variable names

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-echo "${RSA_KEY}" | base64 --decode >/tmp/rsync_rsa
-rsync -avz -e "ssh -i /tmp/rsync_rsa" --delete ${TRAVIS_BUILD_DIR}/dist/ornamentum-demo/ ${USER}@${HOST}:~/ornamentum.app/
+echo "${DEPLOY_RSA_KEY}" | base64 --decode >/tmp/rsync_rsa
+rsync -avz -e "ssh -i /tmp/rsync_rsa" --delete ${TRAVIS_BUILD_DIR}/dist/ornamentum-demo/ ${DEPLOY_USER}@${DEPLOY_HOST}:~/ornamentum.app/
+
 
