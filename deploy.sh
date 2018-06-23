f418f5e9494cc0c5b11a3f4488b921b57ba40886
Yohan Gomez <yohan.gz@hotmail.com>
2018-06-23 22:40:16 +0530

    Fix ssh add command issues

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "start deploy"
-rsync -avz -e "ssh -i ${HOME}/.ssh/id_rsa_deploy" --delete ${TRAVIS_BUILD_DIR}/dist/ornamentum-demo/ ${DEPLOY_USER}@${DEPLOY_HOST}:~/ornamentum.app/
+rsync -avz -e "ssh -i ~/.ssh/id_rsa_deploy" --delete ${TRAVIS_BUILD_DIR}/dist/ornamentum-demo/ ${DEPLOY_USER}@${DEPLOY_HOST}:~/ornamentum.app/
 echo "end deploy"
 
