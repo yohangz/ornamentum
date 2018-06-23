0bcb285f5b1be9845413aa0a5f27c5ea90c16f6a
Yohan Gomez <yohan.gz@hotmail.com>
2018-06-23 20:48:49 +0530

    Add deploy logs

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+echo "start deploy"
 echo "${DEPLOY_RSA_KEY}" | base64 --decode >/tmp/rsync_rsa
-rsync -avz -e "ssh -i /tmp/rsync_rsa" --delete ${TRAVIS_BUILD_DIR}/dist/ornamentum-demo/ ${DEPLOY_USER}@${DEPLOY_HOST}:~/ornamentum.app/
 
+echo "key generation successful"
+echo "deploy build path: ${TRAVIS_BUILD_DIR}/dist/ornamentum-demo/"
+echo "deploy instance: ${DEPLOY_USER}@${DEPLOY_HOST}"
+
+rsync -avz -e "ssh -i /tmp/rsync_rsa" --delete ${TRAVIS_BUILD_DIR}/dist/ornamentum-demo/ ${DEPLOY_USER}@${DEPLOY_HOST}:~/ornamentum.app/
+echo "end deploy"
 
