08f3cafe1c4117a3c427c32b62287303a032a734
Yohan Gomez <yohan.gz@hotmail.com>
2018-06-24 00:14:59 +0530

    Vix version migration script to fully automate version bump

---
+++ version.js
@@ -0,0 +1,15 @@
+const { writeFileSync, readJsonSync, writeJsonSync } = require('fs-extra');
+
+const packageJson = readJsonSync('./package.json');
+writeFileSync('./src/environments/version.ts',
+`// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT!
+export const VERSION = '${packageJson.version}';
+`, { encoding: 'utf-8' });
+
+console.log('Wrote version info to src/environments/version.ts');
+
+const libPackage = readJsonSync('./projects/ornamentum/package.json');
+libPackage.version = String(packageJson.version);
+writeJsonSync('./projects/ornamentum/package.json', libPackage, { spaces: 2, EOL: '\n' });
+
+console.log('Wrote version info to projects/ornamentum/package.json');
