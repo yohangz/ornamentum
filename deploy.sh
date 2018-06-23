95bc5e182d86a57e6da1eb66c914232b56a83702
Yohan Gomez <yohan.gz@hotmail.com>
2018-06-23 21:02:19 +0530

    Apply ssh adon to setup known host

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "start deploy"
 echo "${DEPLOY_RSA_KEY}" | base64 --decode >/tmp/rsync_rsa
-
-echo "key generation successful"
-echo "deploy build path: ${TRAVIS_BUILD_DIR}/dist/ornamentum-demo/"
-echo "deploy instance: ${DEPLOY_USER}@${DEPLOY_HOST}"
-
 rsync -avz -e "ssh -i /tmp/rsync_rsa" --delete ${TRAVIS_BUILD_DIR}/dist/ornamentum-demo/ ${DEPLOY_USER}@${DEPLOY_HOST}:~/ornamentum.app/
 echo "end deploy"
 
