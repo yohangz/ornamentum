5d732c53badf4ce481802567ba72fb172e09ada4
Yohan Gomez <yohan.gz@hotmail.com>
2018-07-01 14:31:53 +0530

    Docker support

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# base image
+FROM node:8.11.3
+
+# base work directory
+WORKDIR /app
+
+# Compy build artifacts
+COPY dist/ornamentum-demo /app
+
+# Expose application via port 8080
+EXPOSE 8080
+
+CMD ["node", "server.js"]
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 echo "start deploy"
-rsync -avz -e "ssh -i ~/.ssh/id_rsa_ornamentum" --delete ${TRAVIS_BUILD_DIR}/dist/ornamentum-demo/ ${DEPLOY_USER}@${DEPLOY_HOST}:~/ornamentum.app/
+
+IMAGE_NAME=${DOCKER_USERNAME}/ornamentum
+
+echo "build docker image"
+docker build --pull --cache-from ${IMAGE_NAME} -t ${IMAGE_NAME} ${TRAVIS_BUILD_DIR}
+
+echo "login to docker hub"
+echo docker ${DOCKER_PASSWORD} | login -u ${DOCKER_USERNAME} --password-stdin
+
+echo "login to docker hub"
+docker tag ${IMAGE_NAME} ${IMAGE_NAME}:latest
+
+echo "push docker image"
+docker push ${IMAGE_NAME}:latest
+
+#rsync -avz -e "ssh -i ~/.ssh/id_rsa_ornamentum" --delete ${TRAVIS_BUILD_DIR}/dist/ornamentum-demo/ ${DEPLOY_USER}@${DEPLOY_HOST}:~/ornamentum.app/
 echo "end deploy"
 
