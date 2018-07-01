55ba2a9bfe01418218804b81d53c6d37b9f7f18f
Yohan Gomez <yohan.gz@hotmail.com>
2018-07-01 17:54:15 +0530

    Publish tagged container via travis support

--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,11 @@ echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
 
 echo "tag docker image"
 docker tag ${IMAGE_NAME} ${IMAGE_NAME}:latest
+docker tag ${IMAGE_NAME} ${IMAGE_NAME}:${TRAVIS_TAG}
 
 echo "push docker image"
 docker push ${IMAGE_NAME}:latest
+docker push ${IMAGE_NAME}:${TRAVIS_TAG}
 
 ssh -i ~/.ssh/id_rsa_ornamentum ${DEPLOY_USER}@${DEPLOY_HOST} "sh deploy.ornamentum.sh"
 
