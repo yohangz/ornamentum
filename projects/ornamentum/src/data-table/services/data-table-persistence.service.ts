a57f22b4ca68bbc48111f3b48f55f9b227ea2a3e
Yohan Gomez <yohan.gz@hotmail.com>
2018-07-02 18:32:48 +0530

    SSR associated lib updates

--- projects/ornamentum/src/data-table/services/data-table-persistence.service.ts
+++ projects/ornamentum/src/data-table/services/data-table-persistence.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import { DataTableRequestParams } from '../models/data-table-request-params.model';
 import { DataTableStorageMode } from '../models/data-table-storage-mode.enum';
@@ -6,19 +6,16 @@ import { DataTableStorageMode } from '../models/data-table-storage-mode.enum';
 import { GlobalRefService } from '../../utility/utility.module';
 import { DataTableConfigService } from './data-table-config.service';
 
-import { isPlatformBrowser } from '@angular/common';
-
 @Injectable()
 export class DataTablePersistenceService {
   private storage: Storage;
 
   constructor(private globalRefService: GlobalRefService,
-              private config: DataTableConfigService,
-              @Inject(PLATFORM_ID) private platformId: Object) {
+              private config: DataTableConfigService) {
   }
 
   public set storageMode(value: DataTableStorageMode) {
-    if (isPlatformBrowser(this.platformId)) {
+    if (this.globalRefService.window) {
       if (value === DataTableStorageMode.LOCAL) {
         this.storage = this.globalRefService.window.localStorage;
       } else {
@@ -28,13 +25,13 @@ export class DataTablePersistenceService {
   }
 
   public setState(id: string, value: DataTableRequestParams) {
-    if (isPlatformBrowser(this.platformId)) {
+    if (this.globalRefService.window) {
       this.storage.setItem(`${this.config.stateKeyPrefix}${id}`, JSON.stringify(value));
     }
   }
 
   public getState(id: string): DataTableRequestParams {
-    if (isPlatformBrowser(this.platformId)) {
+    if (this.globalRefService.window) {
       return JSON.parse(this.storage.getItem(`${this.config.stateKeyPrefix}${id}`));
     }
 
