39297f6547b783fd52cd31089ff66347d1c2a482
Yohan Gomez <yohan.gz@hotmail.com>
2018-07-05 21:36:22 +0530

    Fix sidebar menu animations

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { UtilityModule } from 'ornamentum';
 
@@ -26,7 +25,6 @@ import { SeoGuard } from './seo.guard';
     CommonModule,
     RouterModule,
     HttpClientModule,
-    BrowserAnimationsModule,
     UtilityModule.forRoot()
   ],
   providers: [
