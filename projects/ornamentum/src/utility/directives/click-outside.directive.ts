19ab2e5819f46a9b859a560b326e94d6f16158ae
Yohan Gomez <yohan.gz@hotmail.com>
2018-07-06 12:33:09 +0530

    Fix ClickOutsideDirective not working on ios chrome issue

--- projects/ornamentum/src/utility/directives/click-outside.directive.ts
+++ projects/ornamentum/src/utility/directives/click-outside.directive.ts
@@ -1,6 +1,4 @@
-import {
-  Directive, EventEmitter, HostListener, Output
-} from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostListener, Output } from '@angular/core';
 
 /**
  * Click outside directive.
@@ -10,8 +8,6 @@ import {
   selector: '[ngClickOutside]'
 })
 export class ClickOutsideDirective {
-  private localEvent: Event = null;
-
   /**
    * Click outside event emitter.
    * @type {EventEmitter<Event>}
@@ -19,35 +15,19 @@ export class ClickOutsideDirective {
   @Output()
   public ngClickOutside = new EventEmitter<Event>();
 
+  constructor(private el: ElementRef) {
+  }
+
   /**
    * Track and compare the click event at the document root.
    * @param event Click event
    */
   @HostListener('document: click', ['$event'])
   public compareEvent(event: Event) {
-    // If the event at the document root is the same reference as the
-    // event at the target, it means that the event originated from
-    // within the target and bubbled all the way to the root. As such,
-    // if the event at the document root does NOT MATCH the last known
-    // event at the target, the event must have originated from
-    // outside of the target.
-    if (event !== this.localEvent) {
-      this.ngClickOutside.emit(event);
+    if (this.el.nativeElement && this.el.nativeElement.contains(event.target)) {
+      return;
     }
 
-    this.localEvent = null;
-  }
-
-  /**
-   * Track the click event on the bound target.
-   * @param {Event} event
-   */
-  @HostListener('click', ['$event'])
-  public trackEvent(event: Event) {
-    // When the user clicks inside the bound target, we need to start
-    // tracking the event as it bubbles up the DOM tree. This way,
-    // when a click event hits the document root, we can determine if
-    // the event originated from within the target.
-    this.localEvent = event;
+    this.ngClickOutside.emit(event);
   }
 }
