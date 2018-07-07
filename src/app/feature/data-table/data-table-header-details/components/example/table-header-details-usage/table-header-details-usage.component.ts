3fcf427fa3c81c3372e79769d8d8f885a23264cf
Lahiru Jayamanna <lsampath999@gmail.com>
2018-07-07 13:29:27 +0530

    Fix column selector location not persisting issue.

--- src/app/feature/data-table/data-table-header-details/components/example/table-header-details-usage/table-header-details-usage.component.ts
+++ src/app/feature/data-table/data-table-header-details/components/example/table-header-details-usage/table-header-details-usage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input } from '@angular/core';
 
 import { Observable } from 'rxjs';
 import { of } from 'rxjs';
@@ -11,6 +11,9 @@ import { data } from './table-header-details-usage.data';
 })
 export class TableHeaderDetailsUsageComponent {
   public items: Observable<any>;
+
+  @Input()
+  public parentElement: ElementRef;
 
   constructor() {
     this.items = of(data);
