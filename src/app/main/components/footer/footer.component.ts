5b605bdbd8514e247048d88b0a185044f85186d6
Yohan Gomez <yohan.gz@hotmail.com>
2018-07-09 22:07:01 +0530

    Add main footer component

---
+++ src/app/main/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { VERSION } from '../../../../environments/version';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent {
+  public currentYear: Number;
+  public packageVersion: string;
+
+  constructor() {
+    this.currentYear = (new Date()).getFullYear();
+    this.packageVersion = VERSION;
+  }
+}
