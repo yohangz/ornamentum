ffe691b0e8d2a4594a7bd11b1147f07d78808a25
Yohan Gomez <yohan.gz@hotmail.com>
2018-07-15 19:33:27 +0530

    Refactor sidebar menu nav controls

--- src/app/core/components/left-navigation/left-navigation.component.ts
+++ src/app/core/components/left-navigation/left-navigation.component.ts
@@ -1,8 +1,8 @@
-import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 
 import { Subscription } from 'rxjs/internal/Subscription';
 
-import { MenuGroup, MenuItem, ResizeArgs } from '../../models';
+import { MenuGroup, ResizeArgs } from '../../models';
 import { ContainerResponsiveService } from '../../services';
 
 
@@ -11,19 +11,15 @@ import { ContainerResponsiveService } from '../../services';
   templateUrl: './left-navigation.component.html',
   styleUrls: ['./left-navigation.component.scss']
 })
-export class LeftNavigationComponent implements OnInit, OnDestroy {
+export class LeftNavigationComponent implements OnDestroy {
   private containerResponsiveSubscription: Subscription;
   private navigationToggleSubscription: Subscription;
 
   @Input()
   public menuGroups: MenuGroup[];
 
-  @ViewChild('searchBox')
-  public searchBox: ElementRef;
-
   public containerHeight: number;
   public expanded =  false;
-  public displayMenuGroups: MenuGroup[];
 
   constructor(private containerResponsive: ContainerResponsiveService) {
     this.containerResponsiveSubscription = this.containerResponsive.containerSize.subscribe((resizeArgs: ResizeArgs) => {
@@ -36,34 +32,11 @@ export class LeftNavigationComponent implements OnInit, OnDestroy {
   }
 
   public get menuHeight(): number {
-    return this.containerHeight - this.searchBox.nativeElement.offsetHeight - 32;
+    return this.containerHeight - 32;
   }
 
   public closeMenu(): void {
     this.expanded = false;
-  }
-
-  public onSearchKeyUp(element: any): void {
-    const value = element.value;
-
-    this.displayMenuGroups = this.menuGroups.reduce((acc: MenuGroup[], menuGroup: MenuGroup) => {
-      const items = menuGroup.menuItems.filter((menuItem: MenuItem) => {
-        return menuItem.title.toLowerCase().includes(value.toLowerCase());
-      });
-
-      if (items.length) {
-        acc.push({
-          name: menuGroup.name,
-          menuItems: items
-        });
-      }
-
-      return acc;
-    }, []);
-  }
-
-  public ngOnInit(): void {
-    this.displayMenuGroups = this.menuGroups;
   }
 
   public ngOnDestroy(): void {
