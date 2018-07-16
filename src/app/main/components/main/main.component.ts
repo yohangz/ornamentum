15ec7dd8e33e5dc84386f1c50575897ce2d0d9ab
Samuditha Wadigamangawa <samuditha@live.com>
2018-07-16 18:48:29 +0530

    Updating Theming Files, Adding Footer Styles, Fixing Container Width Issue & Theme Selector Selected Styles

--- src/app/main/components/main/main.component.ts
+++ src/app/main/components/main/main.component.ts
@@ -12,23 +12,23 @@ import { Theme } from '../../../core/models/theme.enum';
   selector: 'app-main',
   styleUrls: ['./main.component.scss'],
   templateUrl: './main.component.html',
-  animations: [
-    trigger('routeFadeIn', [
-      transition('* <=> *', [
-        query(':enter, :leave', style({position: 'fixed', width: '100%'}), {optional: true}),
-        group([
-          query(':enter', [
-            style({opacity: 0}),
-            animate('0.5s ease-in-out', style({opacity: 1}))
-          ], {optional: true}),
-          query(':leave', [
-            style({opacity: 1}),
-            animate('0.5s ease-in-out', style({opacity: 0}))
-          ], {optional: true}),
-        ])
-      ])
-    ])
-  ]
+  // animations: [
+  //   trigger('routeFadeIn', [
+  //     transition('* <=> *', [
+  //       query(':enter, :leave', style({position: 'relative', width: '100%'}), {optional: true}),
+  //       group([
+  //         query(':enter', [
+  //           style({opacity: 0}),
+  //           animate('0.5s ease-in-out', style({opacity: 1}))
+  //         ], {optional: true}),
+  //         query(':leave', [
+  //           style({opacity: 1}),
+  //           animate('0.5s ease-in-out', style({opacity: 0}))
+  //         ], {optional: true}),
+  //       ])
+  //     ])
+  //   ])
+  // ]
 })
 export class MainComponent {
   public themeCssClass: string = Theme.dark;
