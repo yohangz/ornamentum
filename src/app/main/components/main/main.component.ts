ab21986e5fdf666fbfa0852c2379212eb24c1978
Yohan Gomez <yohan.gz@hotmail.com>
2018-07-16 20:18:24 +0530

    Remove animations module references

--- src/app/main/components/main/main.component.ts
+++ src/app/main/components/main/main.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { animate, group, query, style, transition, trigger } from '@angular/animations';
 
 import { Theme } from '../../../core/models/theme.enum';
 
@@ -11,24 +10,7 @@ import { Theme } from '../../../core/models/theme.enum';
 @Component({
   selector: 'app-main',
   styleUrls: ['./main.component.scss'],
-  templateUrl: './main.component.html',
-  // animations: [
-  //   trigger('routeFadeIn', [
-  //     transition('* <=> *', [
-  //       query(':enter, :leave', style({position: 'relative', width: '100%'}), {optional: true}),
-  //       group([
-  //         query(':enter', [
-  //           style({opacity: 0}),
-  //           animate('0.5s ease-in-out', style({opacity: 1}))
-  //         ], {optional: true}),
-  //         query(':leave', [
-  //           style({opacity: 1}),
-  //           animate('0.5s ease-in-out', style({opacity: 0}))
-  //         ], {optional: true}),
-  //       ])
-  //     ])
-  //   ])
-  // ]
+  templateUrl: './main.component.html'
 })
 export class MainComponent {
   public themeCssClass: string = Theme.dark;
