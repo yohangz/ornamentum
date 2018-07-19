5cf7ff87625edffc5297a6eacfbf0f89d3212725
Yohan Gomez <yohan.gz@hotmail.com>
2018-07-19 18:12:47 +0530

    Refactor deployment script

--- server-deploy.sh
+++ server-deploy.sh
@@ -10,42 +10,42 @@ function print() {
 function handleError() {
         if [ "$1" == "0" ]
         then
-                print $2 $Gre
+                print $2 "$Gre"
         else
-                print $3 $Red
+                print $3 "$Red"
                 exit 1
         fi
 }
 
-print "Deploy start" $Gre
+print "Deploy start" "$Gre"
 if docker ps -a | grep "$CONTAINER"
 then
-        print "Container '$CONTAINER' found" $Gre
-        docker stop $CONTAINER
+        print "Container '$CONTAINER' found" "$Gre"
+        docker stop "$CONTAINER"
         handleError $? "Container stop successful" "Container stop failure"
 
-        docker rm $CONTAINER
+        docker rm "$CONTAINER"
         handleError $? "Container remove successful" "Container remove failure"
 else
-        print "Container '$CONTAINER' not found" $Gre
+        print "Container '$CONTAINER' not found" "$Gre"
 fi
 
-docker images -a |  grep $IMAGE
+docker images -a |  grep "$IMAGE"
 handleError $? "Image '$IMAGE' found" "Image '$IMAGE' not found"
 
-docker rmi $IMAGE
+docker rmi "$IMAGE"
 handleError $? "Image removed" "Image remove failure"
 
-docker pull $IMAGE
+docker pull "$IMAGE"
 handleError $? "Image pull successful" "Image pull failure"
 
-docker run --name "$CONTAINER" --restart unless-stopped -p 8080:8080 -d $IMAGE
+docker run --name "$CONTAINER" --restart unless-stopped -p 8080:8080 -d "$IMAGE"
 handleError $? "Docker run successful" "Container run failure"
 
-print "Docker image list" $Gre
+print "Docker image list" "$Gre"
 docker images
 
-print "Docker process list" $Gre
+print "Docker process list" "$Gre"
 docker ps
 
-print "Deploy end" $Gre
+print "Deploy end" "$Gre"
