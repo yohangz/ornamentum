aae58aa1eb7d65bc78dea76b05abd6f68d3703ee
Yohan Gomez <yohan.gz@hotmail.com>
2018-07-19 18:38:37 +0530

    Update deployment scripts with deployment verification and validations

--- server-deploy.sh
+++ server-deploy.sh
@@ -5,30 +5,30 @@ IMAGE="yohangz/ornamentum"
 CONTAINER="ornamentum-container"
 
 function print() {
-        echo -e "\n$2$1$RCol"
+  echo -e "\n$2$1$RCol"
 }
 
 function handleError() {
-        if [ "$1" == "0" ]
-        then
-                print $2 "$Gre"
-        else
-                print $3 "$Red"
-                exit 1
-        fi
+  if [ "$1" == "0" ]
+  then
+    print $2 "$Gre"
+  else
+    print $3 "$Red"
+    exit 1
+  fi
 }
 
 print "Deploy start" "$Gre"
 if docker ps -a | grep "$CONTAINER"
 then
-        print "Container '$CONTAINER' found" "$Gre"
-        docker stop "$CONTAINER"
-        handleError $? "Container stop successful" "Container stop failure"
+  print "Container '$CONTAINER' found" "$Gre"
+  docker stop "$CONTAINER"
+  handleError $? "Container stop successful" "Container stop failure"
 
-        docker rm "$CONTAINER"
-        handleError $? "Container remove successful" "Container remove failure"
+  docker rm "$CONTAINER"
+  handleError $? "Container remove successful" "Container remove failure"
 else
-        print "Container '$CONTAINER' not found" "$Gre"
+  print "Container '$CONTAINER' not found" "$Gre"
 fi
 
 docker images -a |  grep "$IMAGE"
