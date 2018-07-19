f90298dfecd005a2e3c41141c174152ed9b1e71e
Yohan Gomez <yohan.gz@hotmail.com>
2018-07-19 18:38:37 +0530

    Update deployment scripts with deployment verification and validations

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,50 @@
 #!/bin/bash
 
-echo "start publish"
+RCol='\e[0m' ; Red='\e[0;31m'; Yel='\e[0;33m'; Blu='\e[0;34m'; Gre='\e[0;32m'; Pur='\e[0;35m'; Cya='\e[0;36m';
 IMAGE_NAME=${DOCKER_USERNAME}/ornamentum
 
-echo "build docker image"
+function print() {
+  echo -e "\n$2$1$RCol"
+}
+
+function handleError() {
+  if [ "$1" == "0" ]
+  then
+    print $2 "$Gre"
+  else
+    print $3 "$Red"
+    exit 1
+  fi
+}
+
+print "start publish" "$Gre"
+
+print "build docker image" "$Gre"
 docker build --pull --cache-from ${IMAGE_NAME} -t ${IMAGE_NAME} ${TRAVIS_BUILD_DIR}
+handleError $? "docker image build successful" "docker image build failure"
 
-echo "login to docker hub"
+print "login to docker hub" "$Gre"
 echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
+handleError $? "docker login successful" "docker login failure"
 
-echo "tag docker image"
+print "tag docker image" "$Gre"
 docker tag ${IMAGE_NAME} ${IMAGE_NAME}:latest
-docker tag ${IMAGE_NAME} ${IMAGE_NAME}:${TRAVIS_TAG}
+handleError $? "docker latest tag successful" "docker latest tag failure"
 
-echo "push docker image"
+docker tag ${IMAGE_NAME} ${IMAGE_NAME}:${TRAVIS_TAG}
+handleError $? "docker tag successful" "docker tag failure"
+
+print "push docker image" "$Gre"
 docker push ${IMAGE_NAME}:latest
+handleError $? "docker latest push successful" "docker latest push failure"
+
 docker push ${IMAGE_NAME}:${TRAVIS_TAG}
+handleError $? "docker tag push successful" "docker tag push failure"
 
 ssh -i ~/.ssh/id_rsa_ornamentum ${DEPLOY_USER}@${DEPLOY_HOST} "sh deploy.ornamentum.sh"
 
-echo "end publish"
+print "end publish" "$Gre"
+print "deploy verification start" "$Gre"
+
+curl -k -I "https://"${DEPLOY_HOST} 2>&1 | grep "HTTP/2 200"
+handleError $? "deployment successful" "deployment failure"
