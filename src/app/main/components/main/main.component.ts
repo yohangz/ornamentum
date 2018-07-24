fb470819e37d3001ab75909f506a1ae475b77811
Yohan Gomez <yohan.gz@hotmail.com>
2018-07-24 21:44:01 +0530

    Scroll to top on menu item click

--- src/app/main/components/main/main.component.ts
+++ src/app/main/components/main/main.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+import { Subscription } from 'rxjs/internal/Subscription';
 
 import { Theme } from '../../../core/models/theme.enum';
+import { filter } from 'rxjs/operators';
 
 /**
  * Component class for showing main view.
@@ -12,18 +15,32 @@ import { Theme } from '../../../core/models/theme.enum';
   styleUrls: ['./main.component.scss'],
   templateUrl: './main.component.html'
 })
-export class MainComponent {
+export class MainComponent implements OnInit, OnDestroy {
+  public routerSubscription: Subscription;
   public themeCssClass: string = Theme.dark;
+
+  @ViewChild('pageWrapper')
+  public pageWrapper: ElementRef;
 
   constructor(private router: Router) {
   }
 
-  public getState(): string {
-    return this.router.url;
-  }
-
   public onThemeChange(cssClass: string): void {
     this.themeCssClass = cssClass;
+  }
+
+  public ngOnInit(): void {
+    this.routerSubscription = this.router.events.pipe(
+        filter(event => event instanceof NavigationEnd)
+      ).subscribe(() => {
+        if (this.pageWrapper && this.router.url.includes('/feature/')) {
+          this.pageWrapper.nativeElement.scrollTop = 0;
+        }
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this.routerSubscription.unsubscribe();
   }
 }
 
