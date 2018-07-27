0a0aef81eab3cb5a337ee3a52d3d1ac4cc0c2e9f
Yohan Gomez <yohan.gz@hotmail.com>
2018-07-27 23:13:08 +0530

    Fix primary routing slowness issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -43,7 +43,9 @@ const appRoutes: Routes = [
  */
 @NgModule({
   exports: [RouterModule],
-  imports: [RouterModule.forRoot(appRoutes, { initialNavigation: 'enabled' })]
+  imports: [RouterModule.forRoot(appRoutes, {
+    initialNavigation: 'enabled'
+  })]
 })
 export class AppRoutingModule {
 }
