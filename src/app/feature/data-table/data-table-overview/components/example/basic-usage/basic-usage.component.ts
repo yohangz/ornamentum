ae1b19ed94c766a4e9fc7b9a626af53c8588c363
Yohan Gomez <yohan.gz@hotmail.com>
2018-08-01 23:10:00 +0530

    Fix SSR not working issue

--- src/app/feature/data-table/data-table-overview/components/example/basic-usage/basic-usage.component.ts
+++ src/app/feature/data-table/data-table-overview/components/example/basic-usage/basic-usage.component.ts
@@ -1,8 +1,8 @@
-import { Component, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, Input } from '@angular/core';
 
 import { DataTableSelectMode } from 'ornamentum';
 
-import { Observable, of, Subscription } from 'rxjs/index';
+import { Observable, of } from 'rxjs/index';
 
 import { DataFetchService } from '../../../../../../shared/services';
 
@@ -10,41 +10,15 @@ import { DataFetchService } from '../../../../../../shared/services';
   selector: 'app-basic-usage',
   templateUrl: './basic-usage.component.html'
 })
-export class BasicUsageComponent implements OnInit, OnDestroy {
+export class BasicUsageComponent {
   public DataTableSelectMode = DataTableSelectMode;
 
   public dataSource: Observable<any>;
-  private dataSubscription: Subscription;
-
-  private interval: any;
 
   @Input()
   public parentElement: ElementRef;
 
   constructor(private dataFetchService: DataFetchService) {
     this.dataSource = of(this.dataFetchService.fetchData());
-  }
-
-  public ngOnInit(): void {
-    this.interval = setInterval(() => {
-      // this.dataSubscription = this.dataSource.subscribe((data: any[]) => {
-      //   data.forEach((val, index) => {
-      //     let date = val.retailerType;
-      //     let newDate = new Date(date);
-      //
-      //     data[index].retailerType = new Date(newDate.setDate(newDate.getDate() + 1)).toUTCString();
-      //
-      //     data[index].year = Number(val.year) + 0.255;
-      //   });
-      // });
-    }, 1000);
-  }
-
-  public ngOnDestroy(): void {
-    clearInterval(this.interval);
-
-    if (this.dataSubscription) {
-      this.dataSubscription.unsubscribe();
-    }
   }
 }
--- src/app/feature/data-table/data-table-real-time-data-binding/components/example/real-time-data-binding/real-time-data-binding.component.ts
+++ src/app/feature/data-table/data-table-real-time-data-binding/components/example/real-time-data-binding/real-time-data-binding.component.ts
@@ -1,6 +1,5 @@
 import { Component, ElementRef, Input } from '@angular/core';
 
-import { Subscription } from 'rxjs/index';
 import { Observable } from 'rxjs';
 import { of } from 'rxjs';
 
@@ -12,37 +11,11 @@ import { DataFetchService } from '../../../../../../shared/services';
 })
 export class RealTimeDataBindingComponent {
   public dataSource: Observable<any>;
-  private dataSubscription: Subscription;
-
-  private interval: any;
 
   @Input()
   public parentElement: ElementRef;
 
   constructor(private dataFetchService: DataFetchService) {
     this.dataSource = of(this.dataFetchService.fetchData());
-  }
-
-  public ngOnInit(): void {
-    this.interval = setInterval(() => {
-      this.dataSubscription = this.dataSource.subscribe((data: any[]) => {
-        data.forEach((val, index) => {
-          let date = val.retailerType;
-          let newDate = new Date(date);
-
-          data[index].retailerType = new Date(newDate.setDate(newDate.getDate() + 1)).toUTCString();
-
-          data[index].year = Number(val.year) + 0.25;
-        });
-      });
-    }, 100);
-  }
-
-  public ngOnDestroy(): void {
-    clearInterval(this.interval);
-
-    if (this.dataSubscription) {
-      this.dataSubscription.unsubscribe();
-    }
   }
 }
