f96abf35695204958197faa66fc0ff86abdaf8c9
Yohan Gomez <yohan.gz@hotmail.com>
2018-08-04 18:47:10 +0530

    Refactor travis scripts to support PR build

---
+++ scripts/before-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ];
+then
+ echo "setup ssh key"
+ openssl aes-256-cbc -K ${encrypted_48bfcff1b99c_key} -iv ${encrypted_48bfcff1b99c_iv} -in deploy.key.enc -out deploy.key -d
+fi
