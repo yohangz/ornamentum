62bd16099bfeb3fe022295342247240905c59b2c
Yohan Gomez <yohan.gz@hotmail.com>
2018-08-27 11:09:02 +0530

    Update model name

--- src/app/shared/models/index.ts
+++ src/app/shared/models/index.ts
@@ -1,6 +1,6 @@
 export { TabChangeEvent } from './tabset.model';
 export { ExampleFile } from './example-file.model';
 
-export { ExampleData } from './example.data';
+export { ExampleData } from './example-data.model';
 
 export { FileType } from './file-type.enum';
