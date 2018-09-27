7180b8f490aa0576baf40506e1649ce55a6b1a72
Samuditha Wadigamangawa <samuditha@live.com>
2018-09-27 13:15:02 +0530

    Adding New Theme and Updating Theme Variables

--- src/app/core/models/theme.enum.ts
+++ src/app/core/models/theme.enum.ts
@@ -2,5 +2,6 @@ export enum Theme {
   base = 'om-base-theme',
   dark = 'om-dark-theme',
   flat = 'om-flat-theme',
-  ornamentum = 'om-ornamentum-theme'
+  ornamentum = 'om-ornamentum-theme',
+  aegean = 'om-aegean-theme'
 }
