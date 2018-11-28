8e905eee6453b319a754fedb0e05121d231c7f6e
Lahiru Jayamanna <lsampath999@gmail.com>
2018-11-28 09:11:41 +0530

    update data table persistence example section.

--- src/app/feature/data-table/data-table-persist-data/index.ts
+++ src/app/feature/data-table/data-table-persist-data/index.ts
@@ -1,2 +1,5 @@
 export { DataTablePersistDataComponent } from './components/data-table-persist-data.component';
 export { DataPersistenceUsageComponent } from './components/example/data-persistence-usage/data-persistence-usage.component';
+export {
+  DataPersistenceStorageModeUsageComponent
+} from './components/example/data-persistence-storage-mode-usage/data-persistence-storage-mode-usage.component';
