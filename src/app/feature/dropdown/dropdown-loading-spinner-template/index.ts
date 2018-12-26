9e7e34f90ab8eacf195b619a380f08241942302a
Yohan Gomez <yohan.gz@hotmail.com>
2018-12-26 20:02:11 +0530

    Lint fixes

--- src/app/feature/dropdown/dropdown-loading-spinner-template/index.ts
+++ src/app/feature/dropdown/dropdown-loading-spinner-template/index.ts
@@ -1,2 +1,3 @@
+// tslint:disable-next-line
 export { LoadingSpinnerTemplateUsageComponent } from './components/example/loading-spinner-template-usage/loading-spinner-template-usage.component';
 export { DropdownLoadingSpinnerTemplateComponent } from './components/dropdown-loading-spinner-template.component';
--- src/app/feature/dropdown/dropdown-translations/index.ts
+++ src/app/feature/dropdown/dropdown-translations/index.ts
@@ -1,3 +1,4 @@
 export { TranslationsNoDataUsageComponent } from './components/example/translations-no-data-usage/translations-no-data-usage.component';
+// tslint:disable-next-line
 export { AdditionalTranslationsUsageComponent } from './components/example/additional-translations-usage/additional-translations-usage.component';
 export { DropdownTranslationsComponent } from './components/dropdown-translations.component';
