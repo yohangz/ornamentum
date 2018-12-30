8e384f5432bbf351cd6bb8aa925cd0bd7a23586b
Lahiru Jayamanna <lsampath999@gmail.com>
2018-12-30 10:09:44 +0530

    Update dropdown & data table realtime data binding examples.

--- src/app/feature/dropdown/dropdown-real-time-data-binding/components/dropdown-real-time-data-binding.component.ts
+++ src/app/feature/dropdown/dropdown-real-time-data-binding/components/dropdown-real-time-data-binding.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { realTimeDataBindingUsage } from './dropdown-real-time-data-binding.list';
+import { demoSnippet, realTimeDataBindingUsage } from './dropdown-real-time-data-binding.list';
 
 @Component({
   selector: 'app-dropdown-real-time-data-binding-component',
@@ -9,4 +9,5 @@ import { realTimeDataBindingUsage } from './dropdown-real-time-data-binding.list
 })
 export class DropdownRealTimeDataBindingComponent {
   public realTimeDataBindingUsage = realTimeDataBindingUsage;
+  public demoSnippet = demoSnippet;
 }
--- src/app/feature/dropdown/dropdown-real-time-data-binding/components/example/real-time-data-binding/real-time-data-binding.component.ts
+++ src/app/feature/dropdown/dropdown-real-time-data-binding/components/example/real-time-data-binding/real-time-data-binding.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable, of, timer } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { GlobalRefService } from 'ornamentum';
 
 import { ExampleData } from '../../../../../../shared/models';
 
@@ -8,10 +13,28 @@ import { DataFetchService } from '../../../../../../shared/services';
   selector: 'app-real-time-data-binding',
   templateUrl: './real-time-data-binding.component.html'
 })
-export class RealTimeDataBindingComponent {
-  public items: ExampleData[];
+export class RealTimeDataBindingComponent implements OnInit {
+  public dataSource: Observable<ExampleData[]>;
 
-  constructor(private dataFetchService: DataFetchService) {
-    this.items = this.dataFetchService.fetchData();
+  constructor(private dataFetchService: DataFetchService, private globalRefService: GlobalRefService) {
+  }
+
+  public ngOnInit(): void {
+    let offset = 0;
+    if (this.globalRefService.isBrowser) {
+      this.dataSource = timer(0, 2000)
+        .pipe(
+          map(() => {
+            offset += 20;
+            if (offset >= 100) {
+              offset = 0;
+            }
+
+            return this.dataFetchService.fetchData(offset, 20);
+          })
+        );
+    } else {
+      this.dataSource = of(this.dataFetchService.fetchData(offset, 20));
+    }
   }
 }
