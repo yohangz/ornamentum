3daacd4b20b23d00b9fd3570d13f3a5a220b783e
Yohan Gomez <yohan.gz@hotmail.com>
2019-01-03 14:26:31 +0530

    Utility directive documentation comments.

--- projects/ornamentum/src/utility/directives/focus.directive.ts
+++ projects/ornamentum/src/utility/directives/focus.directive.ts
@@ -1,5 +1,9 @@
 import { Directive, ElementRef, OnInit } from '@angular/core';
 
+/**
+ * Element focus directive.
+ * Focus to target element on initialize.
+ */
 @Directive({
   selector: '[ngFocus]'
 })
@@ -7,6 +11,9 @@ export class FocusDirective implements OnInit {
   constructor(private el: ElementRef) {
   }
 
+  /**
+   * On directive initialize.
+   */
   public ngOnInit(): void {
     this.el.nativeElement.focus();
   }
--- projects/ornamentum/src/utility/directives/init.directive.ts
+++ projects/ornamentum/src/utility/directives/init.directive.ts
@@ -1,5 +1,9 @@
 import { Directive, EventEmitter, OnInit, Output } from '@angular/core';
 
+/**
+ * Element initialize directive.
+ * Emit event on element init.
+ */
 @Directive({
   selector: '[ngInit]'
 })
@@ -8,6 +12,9 @@ export class InitDirective implements OnInit {
   @Output()
   public ngInit = new EventEmitter();
 
+  /**
+   * On directive initialize.
+   */
   public ngOnInit(): void {
     this.ngInit.emit();
   }
