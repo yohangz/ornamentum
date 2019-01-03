39d40650af3124e5e89378d6e2569ac28e648b44
Yohan Gomez <yohan.gz@hotmail.com>
2019-01-03 14:21:07 +0530

    Add component loader factory doc comments

--- projects/ornamentum/src/utility/services/popover-component-loader-factory.service.ts
+++ projects/ornamentum/src/utility/services/popover-component-loader-factory.service.ts
@@ -7,6 +7,9 @@ import { ResizeService } from './resize.service';
 
 import { PopoverComponentLoader } from './popover-component-loader.class';
 
+/**
+ * Popover component loader factory service.
+ */
 @Injectable()
 export class PopoverComponentLoaderFactoryService {
   constructor(private componentFactoryResolver: ComponentFactoryResolver,
@@ -15,6 +18,11 @@ export class PopoverComponentLoaderFactoryService {
               private resizeService: ResizeService) {
   }
 
+  /**
+   * Create new component loader.
+   * @param renderer - Angular renderer reference.
+   * @return Popover component loader reference.
+   */
   public createLoader<T>(renderer: Renderer2): ComponentLoader<T> {
     return new PopoverComponentLoader<T>(this.componentFactoryResolver, this.appRef, this.globalRefService, renderer, this.resizeService);
   }
