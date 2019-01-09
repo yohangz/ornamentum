c2fd258cceccf589c50c615e79b7db3a70bd9a44
Yohan Gomez <yohan.gz@hotmail.com>
2019-01-09 17:24:38 +0530

    Documentation comment updates.

--- projects/ornamentum/src/data-table/directives/scroll-element.directive.ts
+++ projects/ornamentum/src/data-table/directives/scroll-element.directive.ts
@@ -4,6 +4,9 @@ import { Subscription } from 'rxjs';
 
 import { DataTableScrollPositionService } from '../services/data-table-scroll-position.service';
 
+/**
+ * Scroll element directive; Set scroll position to target element
+ */
 @Directive({
   selector: '[ngScrollElement]'
 })
@@ -13,12 +16,18 @@ export class ScrollElementDirective implements AfterViewInit, OnDestroy {
   constructor(private el: ElementRef, private scrollPositionService: DataTableScrollPositionService) {
   }
 
+  /**
+   * After component initialize lifecycle event handler
+   */
   public ngAfterViewInit(): void {
     this.scrollPositionSubscription = this.scrollPositionService.scrollPositionStream.subscribe((value) => {
       this.el.nativeElement.scrollLeft = value.scrollLeft;
     });
   }
 
+  /**
+   * Component destroy lifecycle event handler
+   */
   public ngOnDestroy(): void {
     if (this.scrollPositionSubscription) {
       this.scrollPositionSubscription.unsubscribe();
