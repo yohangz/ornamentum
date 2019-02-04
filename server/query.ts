b27e3e2b986396a451183ad707fd78cbd4c1e4cb
Yohan Gomez <yohan.gz@hotmail.com>
2019-02-04 14:15:03 +0530

    Server api dropdown filter support

--- server/query.ts
+++ server/query.ts
@@ -2,54 +2,65 @@ import forOwn from 'lodash/forOwn';
 import get from 'lodash/get';
 import orderBy from 'lodash/orderBy';
 
-import { DataTableQueryField } from 'ornamentum';
+import { DataTableQueryField, DropdownFilter } from 'ornamentum';
 
 export const queryDataByFieldExpression = (data: any[], offset: number, limit: number, fields: any = {}): any => {
-  const filters = [];
-  const sort = {
-    fields: [],
-    orders: []
-  };
+  let result = data;
 
-  forOwn(fields, (value: string, key: string): void => {
-    if (value === '') {
-      return;
-    }
-
-    const fieldPair = value.split('|');
-
-    const filterValue = fieldPair[0];
-    if (filterValue !== '') {
-      filters.push({
-        field: key,
-        values: String(filterValue).split(',')
+  if ('filter' in fields) {
+    if (fields.filter.key && fields.filter.value) {
+      const value = String(fields.filter.value).toLowerCase();
+      result = result.filter((item) => {
+        const key = String(get(item, fields.filter.key)).toLowerCase();
+        return key.includes(value);
       });
     }
+  } else {
+    const filters = [];
+    const sort = {
+      fields: [],
+      orders: []
+    };
 
-    const sortOrder = fieldPair[1];
-    if (sortOrder) {
-      sort.fields.push(key);
-      sort.orders.push(sortOrder);
-    }
-  });
+    forOwn(fields, (value: string, key: string): void => {
+      if (value === '') {
+        return;
+      }
 
-  let result = data;
-  if (filters.length) {
-    result = data.filter((item) => {
-      return filters.every((filter) => {
-        const value = get(item, filter.field);
+      const fieldPair = value.split('|');
 
-        return filter.values.some((filterItem) => {
-          const filterValue = String(filterItem).toLowerCase();
-          const fieldValue = String(value).toLowerCase();
-          return fieldValue.includes(filterValue);
+      const filterValue = fieldPair[0];
+      if (filterValue !== '') {
+        filters.push({
+          field: key,
+          values: String(filterValue).split(',')
+        });
+      }
+
+      const sortOrder = fieldPair[1];
+      if (sortOrder) {
+        sort.fields.push(key);
+        sort.orders.push(sortOrder);
+      }
+    });
+
+    if (filters.length) {
+      result = result.filter((item) => {
+        return filters.every((filter) => {
+          const value = get(item, filter.field);
+
+          return filter.values.some((filterItem) => {
+            const filterValue = String(filterItem).toLowerCase();
+            const fieldValue = String(value).toLowerCase();
+            return fieldValue.includes(filterValue);
+          });
         });
       });
-    });
-  }
+    }
 
-  if (sort.orders.length) {
-    result = orderBy(result, sort.fields, sort.orders);
+    if (sort.orders.length) {
+      result = orderBy(result, sort.fields, sort.orders);
+    }
   }
 
   const items = result.slice(offset, offset + limit);
@@ -61,54 +72,63 @@ export const queryDataByFieldExpression = (data: any[], offset: number, limit: n
   };
 };
 
-export const queryDataByFieldCollection = (data: any[], offset: number, limit: number, fields: DataTableQueryField[]): any => {
+export const queryDataByFieldCollection = (data: any[], offset: number, limit: number, params: DataTableQueryField[]|DropdownFilter): any => {
   let result = data;
 
-  console.log(limit, ' ', offset, ' ', fields);
-  const filterFields = fields.filter(field => field.filterable);
-  if (filterFields.length) {
-    result = data.filter(item => {
-      return filterFields.every((filterColumn: DataTableQueryField) => {
-        const fieldValue = get(item, filterColumn.field);
-        if (fieldValue === undefined) {
-          return true;
-        }
+  if (Array.isArray(params)) {
+    const filterFields = params.filter(field => field.filterable);
+    if (filterFields.length) {
+      result = data.filter(item => {
+        return filterFields.every((filterColumn: DataTableQueryField) => {
+          const fieldValue = get(item, filterColumn.field);
+          if (fieldValue === undefined) {
+            return true;
+          }
 
-        if (Array.isArray(filterColumn.filterValue)) {
-          return (
-            filterColumn.filterValue.length === 0 ||
-            filterColumn.filterValue.includes(fieldValue)
-          );
-        }
+          if (Array.isArray(filterColumn.filterValue)) {
+            return (
+              filterColumn.filterValue.length === 0 ||
+              filterColumn.filterValue.includes(fieldValue)
+            );
+          }
 
-        const value = String(fieldValue).toLowerCase();
-        const filterValue = String(filterColumn.filterValue).toLowerCase();
-        return value.includes(filterValue);
+          const value = String(fieldValue).toLowerCase();
+          const filterValue = String(filterColumn.filterValue).toLowerCase();
+          return value.includes(filterValue);
+        });
       });
+    }
+
+    const sortColumns = params.filter((column: DataTableQueryField) => {
+      return column.sortable && column.sortOrder !== '';
     });
-  }
 
-  const sortColumns = fields.filter((column: DataTableQueryField) => {
-    return column.sortable && column.sortOrder !== '';
-  });
+    if (sortColumns.length) {
+      const orderData = sortColumns.reduce(
+        (accumulator: any, column: DataTableQueryField) => {
+          if (accumulator) {
+            accumulator.fields.push(column.field);
+            accumulator.orders.push(column.sortOrder);
+          }
 
-  if (sortColumns.length) {
-    const orderData = sortColumns.reduce(
-      (accumulator: any, column: DataTableQueryField) => {
-        if (accumulator) {
-          accumulator.fields.push(column.field);
-          accumulator.orders.push(column.sortOrder);
+          return accumulator;
+        },
+        {
+          fields: [],
+          orders: []
         }
+      );
 
-        return accumulator;
-      },
-      {
-        fields: [],
-        orders: []
-      }
-    );
-
-    result = orderBy(result, orderData.fields, orderData.orders);
+      result = orderBy(result, orderData.fields, orderData.orders);
+    }
+  } else {
+    if (params.key && params.value) {
+      result = result.filter((item) => {
+        const key = String(get(item, params.key)).toLowerCase();
+        const value = String(params.value).toLowerCase();
+        return key.includes(value);
+      });
+    }
   }
 
   const items = result.slice(offset, offset + limit);
