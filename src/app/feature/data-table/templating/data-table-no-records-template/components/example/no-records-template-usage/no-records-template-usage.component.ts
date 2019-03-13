720d4c9dbe98ca50c470acb8853971c8156b6c80
Yohan Gomez <yohan.gz@hotmail.com>
2019-03-13 22:23:38 +0530

    Templating documentation example updates

--- src/app/feature/data-table/templating/data-table-no-records-template/components/example/no-records-template-usage/no-records-template-usage.component.ts
+++ src/app/feature/data-table/templating/data-table-no-records-template/components/example/no-records-template-usage/no-records-template-usage.component.ts
@@ -1,17 +1,9 @@
 import { Component } from '@angular/core';
 
-import { ExampleData } from 'helper-models';
-
 @Component({
   selector: 'app-no-records-template-usage',
   templateUrl: './no-records-template-usage.component.html'
 })
 export class NoRecordsTemplateUsageComponent {
-  public items: ExampleData[];
-
-  constructor() {
-    setTimeout(() => {
-      this.items = [];
-    }, 5000);
-  }
+  public items: any[] = [];
 }
