64cbbab9c229ed730e640b22f9ca1bbe8ed412d2
Yohan Gomez <yohan.gz@hotmail.com>
2019-03-13 21:57:13 +0530

    Loading spinner config doc updates

--- src/app/feature/data-table/options/data-table-data-loading-spinner/components/example/data-loading-spinner-usage/data-loading-spinner-usage.component.ts
+++ src/app/feature/data-table/options/data-table-data-loading-spinner/components/example/data-loading-spinner-usage/data-loading-spinner-usage.component.ts
@@ -1,19 +1,9 @@
 import { Component } from '@angular/core';
 
-import { ExampleData } from 'helper-models';
-
-import { DataFetchService } from 'helper-services';
-
 @Component({
   selector: 'app-data-loading-spinner-usage',
   templateUrl: './data-loading-spinner-usage.component.html'
 })
 export class DataLoadingSpinnerUsageComponent {
-  public items: ExampleData[];
-
-  constructor(private dataFetchService: DataFetchService) {
-    setTimeout(() => {
-      this.items = this.dataFetchService.fetchStaticData();
-    }, 5000);
-  }
+  public items: any[];
 }
