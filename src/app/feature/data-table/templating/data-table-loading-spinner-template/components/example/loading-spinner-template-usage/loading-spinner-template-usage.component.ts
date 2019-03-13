670e3e6f4535a59a9f907c7316128aeafed1bb77
Yohan Gomez <yohan.gz@hotmail.com>
2019-03-13 22:23:38 +0530

    Templating documentation example updates

--- src/app/feature/data-table/templating/data-table-loading-spinner-template/components/example/loading-spinner-template-usage/loading-spinner-template-usage.component.ts
+++ src/app/feature/data-table/templating/data-table-loading-spinner-template/components/example/loading-spinner-template-usage/loading-spinner-template-usage.component.ts
@@ -1,19 +1,9 @@
 import { Component } from '@angular/core';
 
-import { ExampleData } from 'helper-models';
-
-import { DataFetchService } from 'helper-services';
-
 @Component({
   selector: 'app-loading-spinner-template-usage',
   templateUrl: './loading-spinner-template-usage.component.html'
 })
 export class LoadingSpinnerTemplateUsageComponent {
-  public items: ExampleData[];
-
-  constructor(private dataFetchService: DataFetchService) {
-    setTimeout(() => {
-      this.items = this.dataFetchService.fetchStaticData();
-    }, 5000);
-  }
+  public items: any[];
 }
