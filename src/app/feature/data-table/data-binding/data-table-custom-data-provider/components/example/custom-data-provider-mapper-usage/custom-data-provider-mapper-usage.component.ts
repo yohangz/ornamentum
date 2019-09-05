51bd8feb85a0badaa68f722dfe597e61de933179
Aravinda Rathnayake <aravindacloud@gmail.com>
2019-09-05 14:38:33 +0530

    Doc. updates for custom data provider mapper example

--- src/app/feature/data-table/data-binding/data-table-custom-data-provider/components/example/custom-data-provider-mapper-usage/custom-data-provider-mapper-usage.component.ts
+++ src/app/feature/data-table/data-binding/data-table-custom-data-provider/components/example/custom-data-provider-mapper-usage/custom-data-provider-mapper-usage.component.ts
@@ -9,7 +9,7 @@ import { DataTableQueryResult, DataTableRequestParams } from 'ornamentum';
 @Component({
   selector: 'app-custom-data-provider-mapper-usage',
   templateUrl: './custom-data-provider-mapper-usage.component.html',
-  styles: ['.highlight { font-weight: bold; color: #4fc1e9}', '.link { color: #ffce54}']
+  styles: ['.highlight { font-weight: bold; color: #4fc1e9 }', '.link { color: #ffce54 }']
 })
 export class CustomDataProviderMapperUsageComponent {
 
@@ -21,7 +21,6 @@ export class CustomDataProviderMapperUsageComponent {
    * example : Firebase Realtime Database or Cloud Firestore
    */
   public onDataBind = (params: DataTableRequestParams): Observable<DataTableQueryResult<any>> => {
-    debugger
     const page = (params.offset + params.limit) / params.limit;
     const perPage = params.limit;
 
@@ -32,20 +31,17 @@ export class CustomDataProviderMapperUsageComponent {
       .append('per_page', '' + perPage);
 
     return this.http.get('https://api.github.com/search/repositories', { params: httpParams })
-      .pipe(
-        map((res: any) => {
+      .pipe(map((res: any) => {
           /**
            * Items collection
            * items: T[];
            * Item count
            * count: number;
            */
-          console.log(res.items);
           return {
             items: res.items,
             count: res.total_count
           };
-        })
-      ) as Observable<any>;
+        })) as Observable<any>;
   }
 }
