951657b55836e8a3e22f3ef5fb5ea729a57bb493
Chanuka Asanka <cagline@gmail.com>
2019-09-09 17:17:16 +0530

    Cell template document updates

--- src/app/feature/data-table/templating/data-table-cell-template/components/example/cell-template-usage/cell-template-usage.component.ts
+++ src/app/feature/data-table/templating/data-table-cell-template/components/example/cell-template-usage/cell-template-usage.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { ExampleData } from 'helper-models';
 
 import { DataFetchService } from 'helper-services';
+import { DataTableRow } from 'ornamentum';
 
 @Component({
   selector: 'app-cell-template-usage',
@@ -11,8 +12,21 @@ import { DataFetchService } from 'helper-services';
 export class CellTemplateUsageComponent {
   public items: ExampleData[];
 
+
   constructor(private dataFetchService: DataFetchService) {
     this.items = this.dataFetchService.fetchStaticData();
+  }
+
+  public onDynamicRowSpanExtract(row: DataTableRow<ExampleData>): number {
+    if (row.item.availableStores && row.item.availableStores.length) {
+      return row.item.availableStores.length;
+    }
+
+    return 1;
+  }
+
+  public getStoreName(item: ExampleData, index: number, field: string) {
+    return item.availableStores && item.availableStores[index] ? item.availableStores[index][field] : '-';
   }
 
 }
