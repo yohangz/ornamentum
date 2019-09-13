003f89a0b9d6c0ada7d10e3288f4c4047147c4a7
Yohan Gomez <yohan.gz@hotmail.com>
2019-09-13 16:07:08 -0700

    Remove compass references

--- src/app/feature/data-table/overview/data-table-basic-usage/components/data-table-basic-usage.list.ts
+++ src/app/feature/data-table/overview/data-table-basic-usage/components/data-table-basic-usage.list.ts
@@ -2,8 +2,6 @@ export const demoSnippet: any = {
   npm: require('!!raw-loader!./docs/npm-install.md'),
   yarn: require('!!raw-loader!./docs/yarn-install.md'),
   moduleImport: require('!!raw-loader!./docs/module-import.md'),
-  npmCompassImport: require('!!raw-loader!./docs/npm-compass-import.md'),
-  yarnCompassImport: require('!!raw-loader!./docs/yarn-compass-import.md'),
   componentMarkup: require('!!raw-loader!./docs/app.component.html.md'),
   componentController: require('!!raw-loader!./docs/app.component.ts.md'),
   cssImport: require('!!raw-loader!./docs/style-css-import.json.md'),
--- src/app/feature/dropdown/overview/dropdown-basic-usage/components/docs/yarn-compass-import.md
+++
@@ -1,1 +0,0 @@
- yarn add compass-mixins --dev
