d2afccd1fce76e2785303d004f57fa1048583ab9
Yohan Gomez <yohan.gz@hotmail.com>
2019-09-20 18:07:38 -0700

    Data table advanced feature documentation SEO support

--- src/app/feature/data-table/advanced/data-table-advanced.routing.module.ts
+++ src/app/feature/data-table/advanced/data-table-advanced.routing.module.ts
@@ -21,8 +21,9 @@ const dataTableAdvancedRoutes: Routes = [
     data: {
       seo: {
         title: 'Ornamentum | Data Table | Functionality | Events',
-        keywords: '<--add keywords here-->',
-        description: '<--add description here-->'
+        keywords: 'events,data table events,data grid events,data table interaction tracker,data table lifecycle',
+        // tslint:disable-next-line
+        description: 'Data table exposed events allow you to hook in to table rendering lifecycle, change behaviors, and capture user interactions.'
       }
     }
   },
@@ -35,8 +36,8 @@ const dataTableAdvancedRoutes: Routes = [
     data: {
       seo: {
         title: 'Ornamentum | Data Table | Functionality | State Persistence',
-        keywords: '<--add keywords here-->',
-        description: '<--add description here-->'
+        keywords: 'state persistence,persistence state,save table state,save grid state,maintain table state,maintain grid state',
+        description: 'Persist data table state across route navigations and browser sessions depending on state persistence mode.'
       }
     }
   },
@@ -49,8 +50,8 @@ const dataTableAdvancedRoutes: Routes = [
     data: {
       seo: {
         title: 'Ornamentum | Data Table | Functionality | Translations',
-        keywords: '<--add keywords here-->',
-        description: '<--add description here-->'
+        keywords: 'translations,localization,translation,localize table content,localize grid content',
+        description: 'Translations feature exposes the capability of localizing the data table control labels and static contents.'
       }
     }
   },
@@ -63,8 +64,9 @@ const dataTableAdvancedRoutes: Routes = [
     data: {
       seo: {
         title: 'Ornamentum | Data Table | Advanced | Configuration Overriding',
-        keywords: '<--add keywords here-->',
-        description: '<--add description here-->'
+        // tslint:disable-next-line
+        keywords: 'configuration,config,configuration override,data table configuration,data table config,root configuration,global configuration',
+        description: 'Data table default behaviors or configurations can be customized in component usage and module root.'
       }
     }
   }
