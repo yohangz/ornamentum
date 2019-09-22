60f7754182d16a9e73b9cdd64c169119fbf86070
Yohan Gomez <yohan.gz@hotmail.com>
2019-09-21 19:31:23 -0700

    Dropdown options documentation update.

--- src/app/feature/dropdown/options/dropdown-item-disabling/index.ts
+++
@@ -1,2 +0,0 @@
-export { ItemDisablingUsageComponent } from './components/example/item-disabling-usage/item-disabling-usage.component';
-export { DropdownItemDisablingComponent } from './components/dropdown-item-disabling.component';
